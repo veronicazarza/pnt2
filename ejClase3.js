cc5dc0305e2776bdaf07b323848356d0898b7c7b
veronicaszarza <veronicazarza.gcba@gmail.com>
2023-04-07 16:30:50 -0300

    Sumar edades de los personajes

--- ejClase3.js
+++ ejClase3.js
@@ -1,3 +1,10 @@
+/** 
+1. Crear un array de objetos con información de personajes de Los Simpsons, cada
+objeto debe contener el nombre, la edad y el rol del personaje. Utilizar el método
+filter para obtener un nuevo array que contenga solamente los personajes
+menores de 18 años.
+**/ 
+
 const simpsons = [
     {
         nombre: 'Marge',
@@ -27,3 +34,9 @@ const simpsons = [
 ];
 
 let miembrosMenores = simpsons.filter(simpson => simpson.edad < 18);
+/**
+ 2.Utilizar el método reduce para sumar las edades de todos los personajes del array
+creado en el Ejercicio 1. Mostrar el resultado en la consola.
+ */
+let totalEdades = simpsons.reduce((suma,simpson) => suma + simpson.edad,0);
+console.log(totalEdades);
